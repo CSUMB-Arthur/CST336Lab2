21ab95a7425e013ca8575f53620be9be1bf93846
CSUMB-Arthur <38265500+CSUMB-Arthur@users.noreply.github.com>
2018-10-30 23:46:19 -0700

    Fist commit
    Basic files for slotmachine.

---
+++ inc/functions.php
@@ -0,0 +1,51 @@
+<?php
+    function displaySymbol($r, $idnum){
+
+        switch($r){
+            case 0:
+                $symbol = "seven";
+                break;
+            case 1:
+                $symbol = "cherry";
+                break;
+            case 2:
+                $symbol = "lemon";
+                break;
+            case 3:
+                $symbol = "bar";
+                break;
+        }
+        echo "<img id='reel$idnum' src='img/$symbol.png' alt='$symbol' title=". ucfirst($symbol) ." width='70'/>";
+    }
+    function displayPoints($r1, $r2, $r3){
+        echo "<div id='output'>";
+        if ($r1 == $r2 && $r1 == $r3){
+            switch ($r1){
+                case 0:
+                    $totalpoints = 1000;
+                    echo "<h1>Jackpot!</h1>";
+                    break;
+                case 1:
+                    $totalpoints = 500;
+                    break;
+                case 2:
+                    $totalpoints = 250;
+                    break;
+                case 3:
+                    $totalpoints = 900;
+                    break;
+            }
+            echo "<h2>You've won $totalpoints points</h2>";
+        } else{
+            echo "<h3> Try again!</h3>";
+        }
+        echo "</div>";
+    }
+    function play(){
+        for ($i = 1; $i < 4; $i++){
+            ${"randval".$i}=rand(3,3);
+            displaySymbol(${"randval".$i},$i);
+        }
+        displayPoints($randval1, $randval2, $randval3);
+    }
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+        <?php
+            include 'inc/functions.php';
+        ?>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+        <div id="main">
+            <?php
+                play();
+            ?>
+            <form>
+                <input type="submit" value="spin!">
+            </form>
+        </div>
+    </body>
+            
+</html>
